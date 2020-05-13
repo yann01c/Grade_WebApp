9441b0a65dd86ae4d460554ecd2e762fc66836ad
Yannic Haas <yannic.haas@spie.com>
2020-05-13 06:07:43 +0000

    deleted

--- group/db_group.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db = new SQLite3('sqlite/webapp.db');
-    $db->exec('CREATE TABLE IF NOT EXISTS "group" (
-        group_id INTEGER PRIMARY KEY,
-        "group" varchar(50))');
-?>
--- sqlite/create_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db = new SQLite3('sqlite/webapp.db');
-    include 'submit/db_grade.php';
-    include 'classes/db_class.php';
-    include 'login/db_login.php';
-    include 'group/db_group.php';
