6e699d00d20a2f97d75bbcb93fa8108a17f8f32c
Yannic Haas <yannic.haas@spie.com>
2020-06-10 15:48:13 +0200

    path changed

--- js/a2hs/register.js
+++ js/a2hs/register.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('/pwa-examples/a2hs/sw.js')
+             .register('js/a2hs/sw.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 
