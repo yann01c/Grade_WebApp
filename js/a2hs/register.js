8202b01ead9be86a75039818d56558dcd9427153
Yannic Haas <yannic.haas@spie.com>
2020-06-15 14:10:39 +0200

    Local Notifications

--- js/a2hs/register.js
+++ js/a2hs/register.js
@@ -6,37 +6,84 @@ if ('serviceWorker' in navigator) {
              .then(function() { console.log('Service Worker Registered'); });
   }
 
-// Ask for Permission
-Notification.requestPermission(function(status) {
-  console.log('Notification permission status:', status);
-});
 
-// Send notification if granted
-function displayNotification() {
-  if (Notification.permission == 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(req) {
-      var options = {
-        body: 'This is the BODY!',
-        icon: '../../images/icons/iphone/icon_iphone_orange.png',
-        vibrate: [0, 100, 0],
-        data: {
-          dateOfArrival: Date.now,
-          primaryKey: 1
-        },
-        actions: [
-          {action: 'explore', title: 'Visit SPIE Grades',
-            icon: '../../images/icons/checkmark.png'},
-          {action: 'close', title: 'Close Notification',
-            icon: '../../images/icons/x.png'},
-        ]
-      };
-      req.showNotification('Test Notification', options);
-    });
+var $status = document.getElementById('status');
+
+if ('Notification' in window) {
+  $status.innerText = Notification.permission;
+}
+
+function requestPermission() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  Notification.requestPermission(function (result) {
+    $status.innerText = result;
+  });
+}
+
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert('Notification API error: ' + err);
   }
 }
 
-displayNotification();
+function persistentNotification() {
+  if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
+    alert('Persistent Notification API not supported!');
+    return;
+  }
+  
+  try {
+    navigator.serviceWorker.getRegistration()
+      .then((reg) => reg.showNotification("Hi there - persistent!"))
+      .catch((err) => alert('Service Worker registration error: ' + err));
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}
+if (Notification.permission == "granted") {
+  setInterval(persistentNotification, 5000);
+} else {
+  console.log("NO");
+}
+// // Ask for Permission
+// Notification.requestPermission(function(status) {
+//   console.log('Notification permission status:', status);
+// });
 
+// // Send notification if granted
+// function displayNotification() {
+//   if (Notification.permission == 'granted') {
+//     navigator.serviceWorker.getRegistration().then(function(req) {
+//       var options = {
+//         body: 'This is the BODY!',
+//         icon: '../../images/icons/iphone/icon_iphone_orange.png',
+//         vibrate: [0, 100, 0],
+//         data: {
+//           dateOfArrival: Date.now,
+//           primaryKey: 1
+//         },
+//         actions: [
+//           {action: 'explore', title: 'Visit SPIE Grades',
+//             icon: '../../images/icons/checkmark.png'},
+//           {action: 'close', title: 'Close Notification',
+//             icon: '../../images/icons/x.png'},
+//         ]
+//       };
+//       req.showNotification('Test Notification', options);
+//     });
+//   }
+// }
 // // Check if subscription was made
 // if ('serviceWorker' in navigator) {
 //   navigator.serviceWorker.register('sw.js').then(function(reg) {
