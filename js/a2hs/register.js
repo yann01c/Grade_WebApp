37811d104e4a738f907db9830d8fc81e3e0a3022
Yannic Haas <yannic.haas@spie.com>
2020-06-15 13:58:58 +0200

    - Firebase

--- js/a2hs/register.js
+++ js/a2hs/register.js
@@ -35,43 +35,45 @@ function displayNotification() {
   }
 }
 
-// Check if subscription was made
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
-    console.log('Service Worker Registered!', reg);
+displayNotification();
 
-    reg.pushManager.getSubscription().then(function(sub) {
-      if (sub === null) {
-        // Update UI to ask user to register for Push
-        console.log('Not subscribed to push service!');
-      } else {
-        // We have a subscription, update the database
-        console.log('Subscription object: ', sub);
-      }
-    });
-  })
-   .catch(function(err) {
-    console.log('Service Worker registration failed: ', err);
-  });
-}
-function subscribeUser() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(reg) {
+// // Check if subscription was made
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('sw.js').then(function(reg) {
+//     console.log('Service Worker Registered!', reg);
 
-      reg.pushManager.subscribe({
-        userVisibleOnly: true
-      }).then(function(sub) {
-        console.log('Endpoint URL: ', sub.endpoint);
-      }).catch(function(e) {
-        if (Notification.permission === 'denied') {
-          console.warn('Permission for notifications was denied');
-        } else {
-          console.error('Unable to subscribe to push', e);
-        }
-      })
-    })
-  }
-}
+//     reg.pushManager.getSubscription().then(function(sub) {
+//       if (sub === null) {
+//         // Update UI to ask user to register for Push
+//         console.log('Not subscribed to push service!');
+//       } else {
+//         // We have a subscription, update the database
+//         console.log('Subscription object: ', sub);
+//       }
+//     });
+//   })
+//    .catch(function(err) {
+//     console.log('Service Worker registration failed: ', err);
+//   });
+// }
+// function subscribeUser() {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.ready.then(function(reg) {
+
+//       reg.pushManager.subscribe({
+//         userVisibleOnly: true
+//       }).then(function(sub) {
+//         console.log('Endpoint URL: ', sub.endpoint);
+//       }).catch(function(e) {
+//         if (Notification.permission === 'denied') {
+//           console.warn('Permission for notifications was denied');
+//         } else {
+//           console.error('Unable to subscribe to push', e);
+//         }
+//       })
+//     })
+//   }
+// }
 
 // let deferredPrompt;
 // const addBtn = document.querySelector('.add-button');
