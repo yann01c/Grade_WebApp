f8b0a036c741310bd11ecbb7679e4b23c6a9baa8
Yannic Haas <yannic.haas@spie.com>
2020-06-29 16:16:47 +0200

    Styling

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,5 +4,6 @@ function dropdown() {
     arrow = document.getElementById("arrow");
     content.style.display = (content.style.display == "block") ? "none" : "block";
     btn.style.backgroundColor = (btn.style.backgroundColor == "black") ? "rgba(255, 255, 255, 0.1)" : "black";
+
     //arrow.style.transform = (arrow.style.transform == "rotate(90deg)") ? "rotate(0deg)" : "rotate(90deg)";
 }
