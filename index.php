56e40cf2749a418b21f219afe4b3113275ee606e
Yannic Haas <yannic.haas@spie.com>
2020-07-01 11:21:34 +0200

    Scalable

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=2, maximum-scale=1, user-scalable=0"/>
     <title>Grades | Submit</title>
     <link rel="stylesheet" href="css/main.css">
     <link rel="icon" type="image/png" href="images/icons/icon_taskbar_transparent.png">
--- overview.php
+++ overview.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
     <title>Grade | Overview</title>
     <link rel="icon" type="image/png" href="images/icons/icon_taskbar_transparent.png">
     <link rel="stylesheet" href="css/main.css">
