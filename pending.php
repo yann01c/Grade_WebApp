1a85ee7d762dbb46f81049bb00af77d511dcdc63
Yannic Haas <yannic.haas@spie.com>
2020-07-02 13:37:42 +0200

    Checkmark animation

--- pending.php
+++ pending.php
@@ -8,6 +8,7 @@
 </head>
 <body id="pending">
     <div class="outer">
+    <div style="z-index: -1000;width: 100%;position:absolute;display:flex;justify-content: center;height:68vh;"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg></div>
     <div style="z-index: -1000;width: 100%;position:absolute;display:flex;justify-content: center;height:20vh;"><h1>Mail sent to</h1></div>
         <div class="inner">
             <div>
