af2c31f1978259094a1cf5b26250cbfea5471847
Yannic Haas <yannic.haas@quickline.ch>
2020-07-03 15:11:20 +0200

    Logout message

--- account.php
+++ account.php
@@ -178,14 +178,10 @@
             <div class="copyright"><a style="text-decoration:none;" href="privacy.php">All Rights Reserved - © SPIE ICS</a></div>
         </div>
     </div>
-    <?php if(isset($_GET['logout'])) {
-        if ($_GET['logout'] == "complete") {
+    <?php if (empty($_SESSION['userID'])) {
             echo "<script>alert('Logout in 20 Seconds...');</script>";
-            header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fgrades-i.spie.ch");
             echo "<script>setTimeout('location.href = 'account.php',20000);</script>";
-            
-        }
-    } ?>
+        } ?>
     <script src="js/a2hs/addtohomescreen.js"></script>
     <script>addToHomescreen();</script>
     <script src="js/a2hs/register.js"></script>
--- login/logout.php
+++ login/logout.php
@@ -6,6 +6,5 @@ session_start();
 setcookie("mod_auth_openidc_session","",time() -3600);
 session_unset();
 session_destroy();
-// header("Location: ../account.php?info=logout");
-header("../account.php?logout=complete");
+header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fgrades-i.spie.ch");
 // }
