557a47a22918a1696fa727227ec06481bfb4b8ea
Yannic Haas <yannic.haas@spie.com>
2020-07-06 08:29:41 +0200

    URL change

--- login/reset_password.php
+++ login/reset_password.php
@@ -120,7 +120,7 @@ if (isset($_POST['reset-password'])) {
             // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
 
-        $script = "<script>window.location.href = ../pending.php?email=$email</script>";
+        $script = "<script>window.location.href = m1igrades.msp.ccsn.ch/pending.php?email=$email</script>";
         echo $script;
         // header("Location: ../pending.php?email=$email");
         // exit();
