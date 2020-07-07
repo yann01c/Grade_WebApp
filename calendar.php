e955d9f399312e30e7ed461753f2aa136394a94c
Yannic Haas <yannic.haas@spie.com>
2020-07-07 11:03:11 +0200

    Logout changes

---
+++ calendar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet/less" type="text/css" href="css/less" /> -->
+    <title>Calendar</title>
+</head>
+<body>
+<iframe src="https://calendar.google.com/calendar/embed?height=700&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FZurich&amp;src=MnZ1NTlqZG9yazRwNDk4NWhxYmF1ODl2ZzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23795548&amp;title=Grades" style="border:solid 1px #777" width="800" height="700" frameborder="0" scrolling="no"></iframe>
+<script src="js/calendar.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.11.1/less.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+</html>
--- login/logout.php
+++ login/logout.php
@@ -6,5 +6,12 @@ session_start();
 setcookie("mod_auth_openidc_session","",time() -3600);
 session_unset();
 session_destroy();
-header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2Fgrades-i.spie.ch");
+if ($_SERVER['REQUEST_URI'] == "m1igrades.msp.ccsn.ch" || $_SERVER['REQUEST_URI'] == "m1pgrades.msp.ccsn.ch") {
+    $server = $_SERVER['REQUEST_URI'];
+    header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2F$server");
+
+} else {
+    $server = $_SERVER['REQUEST_URI'];
+    header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2F$server");
+}
 // }
