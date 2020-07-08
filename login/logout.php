7bd02d85a193aa4ced4501dce9e0afe5f37a4ecc
Yannic Haas <yannic.haas@spie.com>
2020-07-08 14:58:43 +0200

    Server Var

--- login/logout.php
+++ login/logout.php
@@ -7,6 +7,6 @@ setcookie("mod_auth_openidc_session","",time() -3600);
 session_unset();
 session_destroy();
 
-$server = $_SERVER['HTTP_HOST'];
+$server = $_SERVER['HTTP_X_FORWARDED_HOST'];
 header("Location: https://login-i-ng.xtra.netwatch.ch/auth/realms/MSP/protocol/openid-connect/logout?redirect_uri=https%3A%2F%2F$server");
 // }
