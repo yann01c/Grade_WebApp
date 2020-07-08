4e15d2e4ca942a7ba1682f5bab1dcb1a9d51c0fe
Yannic Haas <yannic.haas@spie.com>
2020-07-08 11:14:31 +0200

    Calendar styling

--- calendar.php
+++ calendar.php
@@ -3,9 +3,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
     <link rel="stylesheet/less" type="text/css" href="css/calendar.less" />
-    <title>Calendar</title>
+    <title>Grades | Calendar</title>
 </head>
 <body>
 <div class="container">
